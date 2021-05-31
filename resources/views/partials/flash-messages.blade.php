376efeff01936217cee5c3403faecda9742e7f00
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2021-05-31 22:51:06 +0300

    more compact messahes file

--- resources/views/partials/flash-messages.blade.php
+++ resources/views/partials/flash-messages.blade.php
@@ -8,14 +8,12 @@
             </div>
             @endif
 
-
             @if ($message = Session::get('error'))
             <div class="alert alert-danger alert-block">
                 <button type="button" class="close" data-dismiss="alert">×</button>	
                 {{ $message }}
             </div>
             @endif
-
 
             @if ($message = Session::get('warning'))
             <div class="alert alert-warning alert-block">
@@ -24,14 +22,12 @@
             </div>
             @endif
 
-
             @if ($message = Session::get('info'))
             <div class="alert alert-info alert-block">
                 <button type="button" class="close" data-dismiss="alert">×</button>	
                 {{ $message }}
             </div>
             @endif
-
 
             @if ($errors->any())
             <div class="alert alert-danger">
