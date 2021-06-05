e2d994afe042f1d67c3ec87a6278e4449cdc65fb
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2021-06-05 10:06:23 +0300

    Use match instead of any

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,6 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function() {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/dashboard/profile', [UserProfileController::class, 'index'])->name('profile');
-    Route::any('/dashboard/profile/update', [UserProfileController::class, 'update'])->name('profile.update');
+    Route::match(['get', 'post'],'/dashboard/profile/update', [UserProfileController::class, 'update'])->name('profile.update');
     Route::post('/dashboard/profile/deleteavatar/{id}/{fileName}', [UserProfileController::class, 'deleteavatar'])->name('profile.deleteavatar');
 });
