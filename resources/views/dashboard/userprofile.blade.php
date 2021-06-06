1f0af9b1ccf0822835813fe09dc1b41e62191f0b
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2021-06-06 22:09:01 +0300

    no autocomplete off

--- resources/views/dashboard/userprofile.blade.php
+++ resources/views/dashboard/userprofile.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Edit your profile') }}</div>
                 <div class="card-body">
-                    <form action="{{ route('profile.update') }}" enctype='multipart/form-data' method="post" novalidate autocomplete="off">
+                    <form action="{{ route('profile.update') }}" enctype='multipart/form-data' method="post" novalidate>
                         {{csrf_field()}}
 
                         <div class="form-group with-floating-label">
